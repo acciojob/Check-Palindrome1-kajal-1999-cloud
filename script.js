86e3bf69247fbba51be58915d15a228020fc8f8f
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-06-03 05:15:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 // complete the given function
-
-// complete the given function
-
 function palindrome(str){
-	let newstr=str.toLowerCase(str);
-	let strwithoutspace=newstr.replace(/\s/g, "");
-	let i=0;
-	let j=strwithoutspace.length-1;
+	// changing all cases in lower case
+	var newstr = str.toLowerCase(str) ;
+	// removing spaces from the sentences
+	var removedSpaces = newstr.replace(/\s/g, "");
+	let i = 0 ;
+	let j = removedSpaces.length-1 ;
 	while(i<j){
-		if(strwithoutspace.charAt(i)==strwithoutspace.charAt(j)){
+		if(removedSpaces.charAt(i) === removedSpaces.charAt(j)){
 			i++;
 			j--;
 		}
 		else{
-			return false;
+			return false ;
 		}
 	}
-	return true;
+	return true ;
 }
-module.exports = palindrome
+module.export = palindrome ;
