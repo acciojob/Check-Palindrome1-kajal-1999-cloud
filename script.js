0d7810808e1810e1500c01970bbf14f65589a115
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-06-03 05:04:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // complete the given function
 
-function palindrome(str){
+// complete the given function
 
+function palindrome(str){
+	let newstr=str.toLowerCase(str);
+	let strwithoutspace=newstr.replace(/\s/g, "");
+	let i=0;
+	let j=strwithoutspace.length-1;
+	while(i<j){
+		if(strwithoutspace.charAt(i)==strwithoutspace.charAt(j)){
+			i++;
+			j--;
+		}
+		else{
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
